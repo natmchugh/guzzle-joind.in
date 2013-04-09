39ea7fc535890d383f88c6cae43f83112f7b3f69
Nathaniel McHugh <nmchugh@plus.net>
2013-04-09 22:31:33 +0100

    rename events list add event detail add test

--- tests/Guzzle/JoindIn/Tests/Command/EventDetailTest.php
+++ tests/Guzzle/JoindIn/Tests/Command/EventDetailTest.php
@@ -6,34 +6,35 @@ use Guzzle\Tests\GuzzleTestCase;
 use Guzzle\JoindIn\JoindInClient;
 use Guzzle\Service\Description\ServiceDescription;
 
-class EventListTest extends GuzzleTestCase
+class EventDetailTest extends GuzzleTestCase
 {
-    public function testGetEventsRequest()
+
+    public function testEventDetailRequest()
     {
         $configPath = __DIR__.'/../../../../../src/Guzzle/JoindIn/client.json';
         $description = ServiceDescription::factory($configPath);
         $client = new JoindInClient();
         $client->setDescription($description);
 
-        $command = $client->getCommand('EventsList');
-        $this->setMockResponse($client, 'events.list');
+        $command = $client->getCommand('EventDetail', array('event_id' => 1000));
+        $this->setMockResponse($client, 'event.detail');
         $response = $client->execute($command);
-        $this->assertContains('api.joind.in/v2.1/events', $command->getRequest()->getUrl());
+        $this->assertContains('api.joind.in/v2.1/events/1000', $command->getRequest()->getUrl());
     }
 
-    public function testGetEventsResponse()
+    public function testEventDetailResponse()
     {
         $configPath = __DIR__.'/../../../../../src/Guzzle/JoindIn/client.json';
         $description = ServiceDescription::factory($configPath);
         $client = new JoindInClient();
         $client->setDescription($description);
 
-        $this->setMockResponse($client, 'events.list');
-        $response = $client->EventsList();
-        $this->assertSame(1, $response['meta']['count']);
-
+        $this->setMockResponse($client, 'event.detail');
+        $response = $client->EventDetail(
+            array('event_id' => 1000)
+        );
         $events = $response['events'];
         $event = array_pop($events);
-        $this->assertSame("GeekyConf: Explore, Learn & Enjoy", $event['name']);
+        $this->assertSame("Symfony Live London 2012", $event['name']);
     }
 }
